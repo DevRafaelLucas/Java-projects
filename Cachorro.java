a89404d1b4016a97a994bfe844831488bdbed0c4
DevRafaelLucas <RL95654321@GMAIL.COM>
2024-12-09 11:26:06 -0300

    Update Cachorro.java

--- Cachorro.java
+++ Cachorro.java
@@ -1,6 +1,7 @@
 public class Cachorro {
-
-    /* Exercícios 20 OOP - Criar uma classe "Cachorro" com atributos para nome e métodos comeu, bebeu, latiu e lambeu. 
+    /* 
+    
+    Exercícios 20 OOP - Criar uma classe "Cachorro" com atributos para nome e métodos comeu, bebeu, latiu e lambeu. 
     Não esqueça do Encapsulamento e  exibir as informações. 
 
     Exemplos de exibição:
@@ -13,63 +14,45 @@ public class Cachorro {
 
     // Atributos da classe Cachorro
     private String nome;
-    private String comeu;
-    private String bebeu;
-    private String latiu;
-    private String lambeu;
 
-    //Construtor
-    public Cachorro(String nome, String comeu, String bebeu, String latiu, String lambeu) {
-        this.nome = nome;
-        this.comeu = comeu;
-        this.bebeu = bebeu;
-        this.latiu = latiu;
-        this.lambeu = lambeu;
-    }
 
-    //Método getter e setter para ver e modificar os atributos
-
-    //Método getter
+    //Método Getter e Setter da classe
     public String getNome() {
         return nome;
     }
-    public String getComeu() {
-        return comeu;
-    }
-    public String getBebeu() {
-        return bebeu;
-    }
-    public String getLatiu() {
-        return latiu;
-    }
-    public String getLambeu() {
-        return lambeu;
-    }
-
-    //Método setter
     public void setNome(String nome) {
         this.nome = nome;
     }
-    public void setComeu(String comeu) {
-        this.comeu = comeu;
+
+
+    //Método(ação) da classe carro
+    void comeu() {
+        System.out.println("Comeu mundos ");
     }
-    public void setBebeu(String bebeu) {
-        this.bebeu = bebeu;
+    void bebeu() {
+        System.out.println("Bebeu agua ");
     }
-    public void setLatiu(String latiu) {
-        this.latiu = latiu;
+    void latiu() {
+        System.out.println("Latiu forte ");
     }
-    public void setLambeu(String lambeu) {
-        this.lambeu = lambeu;
+    void lambeu() {
+        System.out.println("lambeu bem ");
     }
 
+    //Método principal
     public static void main(String[] args) {
-        Cachorro cachorro = new Cachorro("Devorador de mundos", "Mundos", "agua", "forte", "bem");
+        Cachorro cachorro = new Cachorro();
+        cachorro.setNome("Devoador de Mundos");
 
-        System.out.println("O cachorro " + cachorro.getNome() + " comeu " + cachorro.getComeu());
-        System.out.println("O cachorro " + cachorro.getNome() + " bebeu " + cachorro.getBebeu());
-        System.out.println("O cachorro " + cachorro.getNome() + " latiu " + cachorro.getLatiu());
-        System.out.println("O cachorro " + cachorro.getNome() + " lambeu " + cachorro.getLambeu());
+        System.out.println("Meu cachorroq " + cachorro.getNome() + ", ele:");
+
+        // Objeto de atributo
+        cachorro.comeu();
+        cachorro.bebeu();
+        cachorro.lambeu();
+        cachorro.latiu();
+
+
     }
-    
 }
+
